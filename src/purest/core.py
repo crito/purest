77e73bbff090f6d57eeb8bc84948360bc95a0805
Christo Buschek <crito@30loops.net>
2011-03-19 01:06:38 +0100

    Added docstrings to methods and fixed an error in WSGI.__iter__.

--- src/purest/core.py
+++ src/purest/core.py
@@ -1,19 +1,40 @@
 """ core.py - Core objects of purest."""
 from purest.app import collectd
 
+class Map(object):
+    """Maps between uri/methods and handlers."""
+    def __init__(self):
+        self._routes = {}
+
+    def add(self, uri, method, handler):
+        """Add a new route to the map.
+
+        :param: uri: string
+        :param: method: string
+        :param: handler: callable
+        """
+        self._routes[uri] = { method: handler }
+
+
 class URIHandler(object):
     """Manage all uri/method routes."""
-    @classmethod
-    def parse(cls, uri, method):
-        """Take a uri and method and parse the the Map object for the right handler."""
+    def __init__(self):
+        self._map = Map()
+
+    def parse(self, uri, method):
+        """Take a uri and method and parse the the Map object for the right handler.i
+
+        :param: uri: string
+        :param: method: string
+        """
         if uri == '/collectd/data' and method == "POST":
             return collectd.Collectd.post
         else:
             return URIHandler.fourofour
 
-    def map_request(self, uri, method):
+    def map(self, uri, method):
         """Map a request using its uri and http method to the right handler."""
-        pass
+        self._map.add('/collectd/data', 'POST', collectd.Collectd.post)
 
     @classmethod
     def fourofour(cls, environ, start_response):
@@ -30,7 +51,9 @@ class WSGI(object):
         self.start_response = start_response
 
     def __iter__(self):
-        handler =  URIHandler.parse(self.environ['PATH_INFO'], self.environ['REQUEST_METHOD'])
+        # Create a new parser and parse the request.
+        parser = URIHandler()
+        handler =  parser.parse(self.environ['PATH_INFO'], self.environ['REQUEST_METHOD'])
         return handler(self.environ, self.start_response)
 
     def __call__(self):
