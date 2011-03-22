a54ab2efd90ddf8c6578760050074e505797a008
Christo Buschek <crito@30loops.net>
2011-03-22 17:26:37 +0100

    Changed the data structure and reduce the document size. Each collection is stored in one database and represents one graph.

--- src/purest/app/collectd.py
+++ src/purest/app/collectd.py
@@ -1,19 +1,48 @@
 import httplib, uuid
 import simplejson as json
+import couchdb
+
+def send_message(host, port, uri, method, body=""):
+    conn = httplib.HTTPConnection(host, port)
+    conn.request(method, uri, body)
+    return conn.getresponse()
 
 class Collectd(object):
     @classmethod
     def post(cls, environ, start_response):
+        """Post a new (time,[values,]) tuple to the backend."""
         body = environ['wsgi.input'].readlines()
+        #couchserver = couchdb.client.Server()
         for data_set in json.loads(''.join(body)):
             uid = str(uuid.uuid4())
-            uri = "/collectd/%s" % uid
-            conn = httplib.HTTPConnection("127.0.0.1", 5984)
-            conn.request("PUT", uri, json.dumps(data_set))
-            res = conn.getresponse()
+            db = "%s-%s_%s-%s_%s" % ('_'.join(data_set['host'].split('.')), 
+                    data_set['plugin'], data_set['plugin_instance'], 
+                    data_set['type'], data_set['type_instance'])
+
+            # Using the couchdb api
+            #try:
+            #    db = couchserver[db]
+            #except couchdb.http.ResourceNotFound:
+            #    couchserver.create(db)
+            #    db = couchserver[db]
+
+            doc = {
+                "time": data_set['time'],
+                "values": data_set['values']
+                }
+            #doc_id, doc_rev = db.save(doc)
+
+            res = send_message("127.0.0.1", "5984", "/%s/%s" % (db, uid), "PUT", json.dumps(doc))
+
+            # If the db doesn't exist. Test for a 400 return code.
+            if res.status == 404:
+                res = send_message("127.0.0.1", "5984", "/%s" % (db), "PUT")
+                # Add here the view to query the values.
+                res = send_message("127.0.0.1", "5984", "/%s/%s" % (db, uid), "PUT", json.dumps(doc))
 
         response_headers = [('Content-type', 'text/plain')]
         status = '200 OK'
 
         start_response(status, response_headers)
         yield ''
+
